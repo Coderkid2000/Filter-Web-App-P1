80a0f43c3a315379349362f4add63db0798308fe
Coderkid2000 <114805555+Coderkid2000@users.noreply.github.com>
2023-07-11 07:43:27 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+
+function takeSnapshot(){
+    save('Funny_filter_app.png');
+}
